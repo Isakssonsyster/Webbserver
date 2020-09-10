59f4d54481b6a9a493dcf8d989bbf670bb259af6
Isakssonsyster <isabella.isaksson@elev.ga.ntig.se>
2020-09-10 16:22:45 +0200

    makingAforum

--- Client/app.js
+++ Client/app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
+const http = require('http')
 const port = 3000
 
 const clientDir = __dirname + "\\Client\\"
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/', (req, res) => res.sendFile(clientDir + "index.html" ))
 app.get('/style', (req, res) => res.sendFile(clientDir + "style2.css" ))
 app.get('/bild1', (req, res) => res.sendFile(clientDir + "209891099-288-k515713.jpg" ))
+
+app.post('/', (req, res) => {
+
+    console.log(req.body.name)
+    console.log(req.body.email)
+    res.redirect('/')
+})
+
 
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
